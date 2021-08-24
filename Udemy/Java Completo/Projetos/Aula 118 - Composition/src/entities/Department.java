d4b81ceaa11f8ea84c379e03e324b73b0d80522d
Samuel Moraes <samuel.moraes@protonmail.com>
2021-08-24 20:38:41 -0300

    24/08

---
+++ Udemy/Java Completo/Projetos/Aula 118 - Composition/src/entities/Department.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Department {
+
+	private String name;
+	
+	//construtor vazio
+	public Department() {
+		
+	}
+
+	//construtor cheio
+	public Department(String name) {
+		this.name = name;
+	}
+
+	//Get e Set
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+	
+	
+}
