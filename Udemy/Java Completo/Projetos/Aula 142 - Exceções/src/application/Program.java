e3a49ff11308eb22249aabb73cfab9abf654aad6
Samuel Moraes <samuel.moraes@protonmail.com>
2021-09-11 22:58:25 -0300

    Aula 142 - Excecoes - solucao ruim

--- Udemy/Java Completo/Projetos/Aula 142 - Exceções/src/application/Program.java
+++ Udemy/Java Completo/Projetos/Aula 142 - Exceções/src/application/Program.java
@@ -36,17 +36,13 @@ public class Program {
 			 checkOut = sdf.parse(sc.next());
 			 
 			 
-			 Date now = new Date();
-			 if (checkIn.before(now) || checkOut.before(now)) {
-				 System.out.println("Erro in reservation: Reservation dates for update must be future");
+			String error = reservation.updateDate(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);				 
 			 }
-			 else if (!checkOut.after(checkIn)) {
-				 System.out.println("Error in reservation: Check-out date must be after check-in date");
-			 }
-			 else {
-				 reservation.updateDate(checkIn, checkOut);
-				 System.out.println("Reservation: " + reservation); 
-			 }
+			else {
+				System.out.println("Reservation: " + reservation); 
+			}
 	
 		 }
  
