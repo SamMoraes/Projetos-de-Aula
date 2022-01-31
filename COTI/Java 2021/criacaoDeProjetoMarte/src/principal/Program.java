cd83541dc33e11a116b726f3802c03526507b523
Samuel Moraes <sm.smoraes@outlook.com>
2022-01-31 19:29:25 -0300

    1

--- COTI/Java 2021/criacaoDeProjetoMarte/src/principal/Program.java
+++ COTI/Java 2021/criacaoDeProjetoMarte/src/principal/Program.java
@@ -10,22 +10,22 @@ public class Program {
 
 	public static void main(String[] args) throws Exception {
 
-			boolean isValid = true;
-		  Project p = new Project();
-		  TdProjectRepository tdProject = new TdProjectRepository();
-		  
-		  try {
-			  p.setNome(JOptionPane.showInputDialog("Insira o nome do projeto:"));
-			  tdProject.createFolder(p);
-			  tdProject.copyStructure(p);
-			  PermissionFactory.permissionProject();
-			  Thread.sleep (5000);
-			  tdProject.remaneFolder(p);
-			  if (isValid) {
-				  JOptionPane.showMessageDialog(null, "Projeto criado com sucesso");
-			  }
-			  
-} catch (Exception e) {
+		boolean isValid = true;
+		Project p = new Project();
+		TdProjectRepository tdProject = new TdProjectRepository();
+
+		try {
+			p.setNome(JOptionPane.showInputDialog("Insira o nome do projeto:"));
+			tdProject.createFolder(p);
+			tdProject.copyStructure(p);
+			PermissionFactory.permissionProject();
+			Thread.sleep(15000);
+			tdProject.remaneFolder(p);
+			if (isValid) {
+				JOptionPane.showMessageDialog(null, "Projeto criado com sucesso");
+			}
+
+		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
 	}
