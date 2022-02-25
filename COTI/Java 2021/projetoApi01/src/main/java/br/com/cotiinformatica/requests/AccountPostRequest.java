e8a25d17612c1720f7b7f0624a2b604842a62cac
Samuel Moraes <43325441+SamMoraes@users.noreply.github.com>
2022-02-24 21:46:35 -0300

    Aula 19

---
+++ COTI/Java 2021/projetoApi01/src/main/java/br/com/cotiinformatica/requests/AccountPostRequest.java
@@ -0,0 +1,17 @@
+package br.com.cotiinformatica.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AccountPostRequest {
+
+	private String nome;
+	private String login;
+	private String senha;
+
+}
+
+
+
